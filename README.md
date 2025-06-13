cbcfaa33a558ee82efb85fc7fe80d3d14905a5b4
sirbread <gverma0824@outlook.com>
2025-06-12 20:08:28 -0600

    maximum stealth ong (window can be hidden + execulte with alt+a) also pyqt is a fucking disgrace to ui design

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # shush
+
+##todo
+features:
+- check all/uncheck all
+- json loading/save and load json
+
+modules:
+- a
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
+import threading
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QCheckBox, QPushButton
+from pynput import keyboard
 
 from screen_cleaner.desktop_cleanup import (
     hide_desktop_icons,
@@ -11,12 +13,12 @@ from screen_cleaner.desktop_cleanup import (
 class ShushApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("shush")
-        self.setFixedSize(300, 200)
+        self.setWindowTitle("Shush")
+        self.setFixedSize(300, 250)
 
         self.layout = QVBoxLayout()
 
-        self.clear_desktop_checkbox = QCheckBox("clear desktop")
+        self.clear_desktop_checkbox = QCheckBox("Clear Desktop")
         self.layout.addWidget(self.clear_desktop_checkbox)
 
         self.execute_button = QPushButton("EXECUTE")
@@ -27,7 +29,16 @@ class ShushApp(QWidget):
         self.revert_button.clicked.connect(self.revert_selected_actions)
         self.layout.addWidget(self.revert_button)
 
+        self.hide_button = QPushButton("HIDE SHUSH")
+        self.hide_button.clicked.connect(self.enter_stealth_mode)
+        self.layout.addWidget(self.hide_button)
+
         self.setLayout(self.layout)
+
+        self.is_active = False
+
+        self.listener_thread = threading.Thread(target=self.start_hotkey_listener, daemon=True)
+        self.listener_thread.start()
 
     def run_selected_actions(self):
         if self.clear_desktop_checkbox.isChecked():
@@ -38,6 +49,24 @@ class ShushApp(QWidget):
         if self.clear_desktop_checkbox.isChecked():
             show_desktop_icons()
             restore_wallpaper()
+
+    def enter_stealth_mode(self):
+        self.hide()
+
+    def on_activate(self):
+        if not self.is_active:
+            if self.clear_desktop_checkbox.isChecked():
+                self.run_selected_actions()
+                self.is_active = True
+        else:
+            self.revert_selected_actions()
+            self.is_active = False
+
+    def start_hotkey_listener(self):
+        with keyboard.GlobalHotKeys({
+            '<alt>+a': self.on_activate
+        }) as listener:
+            listener.join()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
